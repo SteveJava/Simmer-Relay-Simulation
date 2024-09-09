90518f5a5fadeaf8fecdfb4dd65d8152578ba813
Zakwan Khan <zakwankhan2003@gmail.com>
2024-09-09 18:43:43 +0200

    Got rest of the swimmers to load in

--- src/medleySimulation/FinishCounter.java
+++ src/medleySimulation/FinishCounter.java
@@ -1,36 +1,44 @@
 // Simple class to record when someone has crossed the line first and wins
 package medleySimulation;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class FinishCounter {
-	private boolean firstAcrossLine; //flag
-	private int winner; //who won
-	private int winningTeam; //counter for patrons who have left the club
-	
-	FinishCounter() { 
-		firstAcrossLine= true;//no-one has won at start
-		winner=-1; //no-one has won at start
-		winningTeam=-1; //no-one has won at start
+	private boolean firstAcrossLine; // flag
+	private int winner; // who won
+	private int winningTeam; // counter for patrons who have left the club
+
+	FinishCounter() {
+		firstAcrossLine = true;// no-one has won at start
+		winner = -1; // no-one has won at start
+		winningTeam = -1; // no-one has won at start
 	}
-		
-	//This is called by a swimmer when they touch the fnish line
+
+	// This is called by a swimmer when they touch the fnish line
 	public synchronized void finishRace(int swimmer, int team) {
-		boolean won =false;
-		if(firstAcrossLine) {
-			firstAcrossLine=false;
+		boolean won = false;
+		if (firstAcrossLine) {
+			firstAcrossLine = false;
 			won = true;
-			}
+		}
 		if (won) {
-			winner=swimmer;
-			winningTeam=team;
+			winner = swimmer;
+			winningTeam = team;
 		}
 	}
-	
-	//Has race been won?
+
+	// Has race been won?
 	public boolean isRaceWon() {
 		return !firstAcrossLine;
 	}
 
-	public int getWinner() { return winner; }
-	
-	public int getWinningTeam() { return winningTeam;}
+	public int getWinner() {
+		return winner;
+	}
+
+	public int getWinningTeam() {
+		return winningTeam;
+	}
 }
