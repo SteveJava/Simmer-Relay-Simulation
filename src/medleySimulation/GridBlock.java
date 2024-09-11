f21b542b5b8976049263ce1e831d7df9304c06f5
Zakwan Khan <zakwankhan2003@gmail.com>
2024-09-11 11:27:28 +0200

    Added correct code done on a separate machine; Replaced all files; Added Make file

--- src/medleySimulation/GridBlock.java
+++ src/medleySimulation/GridBlock.java
@@ -4,56 +4,55 @@
 
 package medleySimulation;
 
+
 public class GridBlock {
-
-	private int isOccupied;
-
-	private final boolean isStart; // is this a starting block?
-	private int[] coords; // the coordinate of the block.
-
+	
+	private int isOccupied; 
+	
+	private final boolean isStart;  //is this a starting block?
+	private int [] coords; // the coordinate of the block.
+	
 	GridBlock(boolean startBlock) throws InterruptedException {
-		isStart = startBlock;
-		isOccupied = -1;
+		isStart=startBlock;
+		isOccupied= -1;
 	}
-
+	
 	GridBlock(int x, int y, boolean startBlock) throws InterruptedException {
 		this(startBlock);
-		coords = new int[] { x, y };
+		coords = new int [] {x,y};
 	}
-
-	public synchronized int getX() {
-		return coords[0];
-	}
-
-	public synchronized int getY() {
-		return coords[1];
-	}
-
-	// Get a block
+	
+	public synchronized int getX() {return coords[0];}  
+	
+	public synchronized int getY() {return coords[1];}
+	
+	
+	
+	//Get a block
 	public synchronized boolean get(int threadID) throws InterruptedException {
-		if (isOccupied == threadID)
-			return true; // thread Already in this block
-		if (isOccupied >= 0)
-			return false; // space is occupied
-		isOccupied = threadID; // set ID to thread that had block
+		if (isOccupied==threadID) return true; //thread Already in this block
+		if (isOccupied>=0) return false; //space is occupied
+		isOccupied= threadID;  //set ID to thread that had block
 		return true;
 	}
-
-	// release a block
+		
+	
+	//release a block
 	public synchronized void release() {
-		isOccupied = -1;
+		isOccupied= -1;
 	}
+	
 
-	// is a bloc already occupied?
+	//is a bloc already occupied?
 	public synchronized boolean occupied() {
-		if (isOccupied == -1)
-			return false;
+		if(isOccupied==-1) return false;
 		return true;
 	}
-
-	// is a start block
+	
+	
+	//is a start block
 	public synchronized boolean isStart() {
-		return isStart;
+		return isStart;	
 	}
 
 }
--- src/medleySimulation/SwimTeam.java
+++ src/medleySimulation/SwimTeam.java
@@ -2,44 +2,66 @@
 //Class to represent a swim team - which has four swimmers
 package medleySimulation;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 import medleySimulation.Swimmer.SwimStroke;
 
 public class SwimTeam extends Thread {
+	
+	private CountDownLatch[] latches;
+	private static CyclicBarrier barrierStart;
 
-	public static StadiumGrid stadium; // shared
-	private Swimmer[] swimmers;
-	private int teamNo; // team number
+	public static StadiumGrid stadium; //shared 
+	private Swimmer [] swimmers;
+	private int teamNo; //team number 
 
-	public static final int sizeOfTeam = 4;
-
-	SwimTeam(int ID, FinishCounter finish, PeopleLocation[] locArr) {
-		this.teamNo = ID;
-
-		swimmers = new Swimmer[sizeOfTeam];
-		SwimStroke[] strokes = SwimStroke.values(); // Get all enum constants
-		stadium.returnStartingBlock(ID);
-
-		for (int i = teamNo * sizeOfTeam, s = 0; i < ((teamNo + 1) * sizeOfTeam); i++, s++) { // initialise swimmers in
-																								// team
-			locArr[i] = new PeopleLocation(i, strokes[s].getColour());
-			int speed = (int) (Math.random() * (3) + 30); // range of speeds
-			swimmers[s] = new Swimmer(i, teamNo, locArr[i], finish, speed, strokes[s]); // hardcoded speed for now
-		}
+	public int getID() {
+		return teamNo;
 	}
 
-	public void run() {
-		try {
-			for (int s = 0; s < sizeOfTeam; s++) { // start swimmer threads
-				swimmers[s].start();
+	public static final int sizeOfTeam=4;
+	
+	SwimTeam( int ID, FinishCounter finish,PeopleLocation [] locArr, CyclicBarrier barrier) {
+		this.teamNo=ID;
+		barrierStart=barrier;
 
+		latches = new CountDownLatch[sizeOfTeam];
+		for(int i = 0; i < sizeOfTeam; i++){
+			latches[i]= new CountDownLatch(1);
+		}
+
+		swimmers= new Swimmer[sizeOfTeam];
+	    SwimStroke[] strokes = SwimStroke.values();  // Get all enum constants
+		stadium.returnStartingBlock(ID);
+
+		for(int i=teamNo*sizeOfTeam,s=0;i<((teamNo+1)*sizeOfTeam); i++,s++) { //initialise swimmers in team
+			locArr[i]= new PeopleLocation(i,strokes[s].getColour());
+	      	int speed=(int)(Math.random() * (3)+30); //range of speeds 
+
+			if(strokes[s].getOrder() == 1) {
+				swimmers[s] = new Swimmer(i,this, locArr[i],finish,speed,strokes[s], null, latches[s], barrierStart); //hardcoded speed for now
+			} else {
+				swimmers[s] = new Swimmer(i,this,locArr[i],finish,speed,strokes[s], latches[s-1], latches[s], barrierStart); //hardcoded speed for now
 			}
-
-			for (int s = 0; s < sizeOfTeam; s++)
-				swimmers[s].join(); // don't really need to do this;
-
+		}
+	}
+	
+	
+	public void run() {
+		try {	
+			for(int s=0;s<sizeOfTeam; s++) { //start swimmer threads
+				swimmers[s].start();
+				Thread.sleep(3000);
+			}
+			
+			for(int s=0;s<sizeOfTeam; s++) swimmers[s].join();			//don't really need to do this;
+			
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 }
+	
+
